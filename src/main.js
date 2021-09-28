24ee06191560e868734920953f421a049e623956
洋洋洋专属 <yuyang@zonsure.com>
2021-09-28 18:03:12 +0800

    这是未完成的项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 element-ui
+import ElementUI from 'element-ui'
+// 同时引入 element-ui 的样式主题文件
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册为Vue的插件使用
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import Mail from '@/views/mail/index'
 
 Vue.use(VueRouter)
-
-const routes = []
+const routes = [
+  {
+    path: '/mail',
+    name: 'mail',
+    component: () => import(/* webpackChunkName: 'mail' */'@/views/mail/index')
+  }
+]
 
 const router = new VueRouter({
   routes

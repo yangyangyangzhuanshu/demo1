c2adb3db030f24d956b39e7d3bc2a8e2968964a7
洋洋洋专属 <yuyang@zonsure.com>
2021-09-28 16:01:33 +0800

    这个删除初始组件和修改了文件的版本

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    根组件
     <router-view/>
   </div>
 </template>
